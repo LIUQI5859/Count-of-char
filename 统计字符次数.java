42d113fefb3b8b83da45b3e7b4ce6178ff65c079
LIUQI5859 <97341201+LIUQI5859@users.noreply.github.com>
2022-01-11 14:24:51 +0800

    Add files via upload

---
+++ 统计字符次数.java
@@ -0,0 +1,29 @@
+package com.itheima;
+
+import java.util.Scanner;
+
+public class 统计字符次数 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入一个字符串：");
+        String line = sc.nextLine();
+
+        int bigCount = 0;
+        int smallCount = 0;
+        int numberCount = 0;
+        for (int i=0; i<line.length(); i++) {
+            char ch = line.charAt(i);
+            if (ch>='A' && ch<='Z') {
+                bigCount++;
+            }else if(ch>='a' && ch<='z') {
+                smallCount++;
+            }else if (ch>='0' && ch<='9') {
+                numberCount++;
+            }
+        }
+        System.out.println("大写字母个数：" + bigCount);
+        System.out.println("小写字母个数：" + smallCount);
+        System.out.println("数字个数：" + numberCount);
+    }
+}
